b5ad4055157bb4ecbc30e5e7516e3be088b8a7b3
Luke <lukevgomes@gmail.com>
2015-10-08 12:14:44 +0530

    f

---
+++ src/org/luke/Queue4mLinkedList.java
@@ -0,0 +1,20 @@
+package org.luke;
+
+public class Queue4mLinkedList {
+	
+	class Node {
+	    public int data;
+	    public Node next;
+	 
+	    public Node(int data){
+	           this.data = data;
+	    }
+	 
+	    public void displayNode() {
+	           System.out.print( data + " ");
+	    }
+	}
+	
+	
+
+}
